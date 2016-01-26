8003fe18358024a923d386b656b770dcf5d95fde
slam24 <luismartinez242011@gmail.com>
2016-01-26 11:08:34 -0600

    bug: comments ajax

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,10 +2,7 @@
 
   $(document).on('ajax:success', 'form#comments-form', function(ev, data) {
     console.log(data);
-    $('#comments-box').append('<tr>' +
-    	'<td>' + data.user.email + '</td>'+
-    	 '<td>' + data.article.title + '</td>'+
-    	 '<td>' + data.body + '</td>')+'<tr>';
+    $('#comments-box').append('<tr><td>' + data.user.email + '</td><td>' + data.article.title + '</td><td>' + data.body + '</td><tr>');
     $(this).find('textarea').val('');
   });
 
