14e65311d95b67ca28bc6ccca82e4c6bad525473
slam24 <luismartinez242011@gmail.com>
2016-01-30 16:24:15 -0600

    feat: grvatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ActiveRecord::Base
   has_many :articles  
   has_many :comments 
   include PermissionsConcern
+
+  def avatar
+		 
+		# get the email from URL-parameters or what have you and make lowercase
+		email_address = self.email.downcase
+		 
+		# create the md5 hash
+		hash = Digest::MD5.hexdigest(email_address)
+		 
+		# compile URL which can be used in <img src="RIGHT_HERE"...
+		image_src = "http://www.gravatar.com/avatar/#{hash}"
+  end
 end
